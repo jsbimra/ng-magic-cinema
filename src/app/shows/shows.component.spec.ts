1b611120b6194264e7455c5eba9affcfea5467cc
jsbimra <jsbimra@users.noreply.github.com>
2019-04-03 01:01:30 +0530

    Fixig test cases shows component

--- src/app/shows/shows.component.spec.ts
+++ src/app/shows/shows.component.spec.ts
@@ -1,29 +1,46 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { ShowsComponent } from './shows.component';
 import { HypenTransformPipe } from './../hypen-transform.pipe';
 import { CinemaService } from '../services/cinema.service';
 import { routes } from '../app.routes';
+import { ShowComponent } from './show/show.component';
+import { TicketBookedComponent } from './show/ticket-booked/ticket-booked.component';
+import { OwnerComponent } from '../owner/owner.component';
+import { TicketTypeComponent } from './show/ticket-type/ticket-type.component';
 
 describe('ShowsComponent', () => {
   let component: ShowsComponent;
   let fixture: ComponentFixture<ShowsComponent>;
   let hypenCasePipe: HypenTransformPipe;
   let cinemaService: CinemaService;
-  let showListAnchorDebugElement: DebugElement[]; 
+  let showListAnchorDebugElement: DebugElement[];
+  let router: Router;
+  let location: Location;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         ShowsComponent,
+        ShowComponent,
+        TicketBookedComponent,
+        OwnerComponent,
+        TicketTypeComponent,
         HypenTransformPipe],
       imports: [RouterTestingModule.withRoutes(routes)],
       providers: [CinemaService]
     });
 
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
+    router.initialNavigation();
   }));
 
   beforeEach(async () => {
@@ -38,7 +55,7 @@ describe('ShowsComponent', () => {
 
     fixture.detectChanges();
 
-    showListAnchorDebugElement = fixture.debugElement.queryAll(By.css('.shows-list a'));
+    showListAnchorDebugElement = fixture.debugElement.queryAll(By.css('.show-link'));
 
   });
 
@@ -46,8 +63,8 @@ describe('ShowsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should cinemaService injected via inject() and via testBed.get() both the instance should be matched', ()=>{
-    
+  it('should cinemaService injected via inject() and via testBed.get() both the instance should be matched', () => {
+
     //Injected service via inject method of angular testing and this is dependency injection syntax like angular 1
     inject([CinemaService], (injectCinemaService: CinemaService) => {
       expect(cinemaService).toBe(injectCinemaService);
@@ -58,7 +75,7 @@ describe('ShowsComponent', () => {
     // }
   });
 
-  it('should get all the shows and not to be null from service using getAllShows method ', ()=> {
+  it('should get all the shows and not to be null from service using getAllShows method ', () => {
     let shows = cinemaService.getAllShows();
     console.log(shows);
     expect(shows).not.toBeNull();
@@ -67,7 +84,6 @@ describe('ShowsComponent', () => {
   it('should display all shows names in .show-list a elment matching with each shows array value', () => {
     let shows = cinemaService.getAllShows();
     showListAnchorDebugElement.map((ele, idx) => {
-      console.log(ele.nativeElement)
       expect(ele.nativeElement.textContent).toContain(shows[idx].name);
     })
   });
@@ -76,13 +92,35 @@ describe('ShowsComponent', () => {
     let shows = cinemaService.getAllShows();
     let pipe = new HypenTransformPipe();
     showListAnchorDebugElement.map((ele, idx) => {
-      console.log('ele.nativeElement.href ', ele.nativeElement.href);
+      // console.log('ele.nativeElement.href ', ele.nativeElement.href);
       let transformValue = pipe.transform(shows[idx].name);
-      console.log('transformValue ', transformValue);
+      // console.log('transformValue ', transformValue);
       expect(ele.nativeElement.href).toContain(transformValue);
     })
   });
 
+  it('it should redirect to specific routes dynamically on shows-list link element clicked', fakeAsync(() => {
+    let shows = cinemaService.getAllShows();
+    let pipe = new HypenTransformPipe();
 
+    //With single element expect
+    // let firstLink = fixture.debugElement.query(By.css('.show-link')).nativeElement;
+    // firstLink.click();
+    //  //We wait for all pending promises to be resolved.
+    // tick();
+    // console.log('FINAL: location.path() EXPECT ', location.path());
+    // expect(location.path()).toBe(`/shows/${transformValue}`);
+
+    //with list of element expect
+    showListAnchorDebugElement.map((ele, idx) => {
+      let transformValue = pipe.transform(shows[idx].name);
+      // console.log('transformValue before AWAIT ', transformValue);
+      ele.nativeElement.click();
+      tick();
+      console.log('FINAL: location.path() EXPECT ', location.path());
+      expect(location.path()).toBe(`/shows/${transformValue}`);
+    });
+
+  }));
 
 });
